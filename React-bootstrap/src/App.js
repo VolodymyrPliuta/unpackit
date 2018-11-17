d60cb56d012472203a21fa5ebef0787ca50eeef2
americanman <snowboardracer7@gmail.com>
2018-11-17 16:04:05 -0500

    Fix navbar layout

--- React-bootstrap/src/App.js
+++ React-bootstrap/src/App.js
@@ -7,22 +7,22 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <Navbar>
-          <Navbar.Header>
-            <Navbar>
-              <h1>Think outside the box</h1>
-            </Navbar>
-            <Navbar.Toggle />
-          </Navbar.Header>
-          <Navbar.Collapse>
-            <Navbar.Form pullLeft>
-              <FormGroup>
-                <FormControl type="text" placeholder="Search" />
-              </FormGroup>{' '}
-              <Button type="submit">Submit</Button>
-            </Navbar.Form>
-          </Navbar.Collapse>
-        </Navbar>
+          <Navbar>
+            <Navbar.Header>
+              <Navbar>
+                <h1>Find my favorite item</h1>
+              <Navbar.Form pullLeft>
+                <FormGroup>
+                  <FormControl type="text" placeholder="Search" />
+                </FormGroup>{' '}
+                <Button type="submit">Submit</Button>
+              </Navbar.Form>
+              </Navbar>
+              <Navbar.Toggle />
+            </Navbar.Header>
+            <Navbar.Collapse>
+            </Navbar.Collapse>
+          </Navbar>
 
         <Grid>
           <Row>
@@ -36,7 +36,10 @@ class App extends Component {
               <Image src="https://source.unsplash.com/sxNt9g77PE0/250x250" rounded />
             </Col>
           </Row>
-      </Grid>;
+        </Grid>
+
+        {/*    <h1>Thing outside of the box</h1> */}
+
       </div>
     );
   }
