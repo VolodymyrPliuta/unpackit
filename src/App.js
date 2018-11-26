7e618f886ca093bea3b2e7ffaa582ec825039979
michaeldimmitt <michaelgdimmitt@gmail.com>
2018-11-26 11:43:48 -0500

    mapping logic working to get box and get value.

--- src/App.js
+++ src/App.js
@@ -1,53 +1,62 @@
 import React, { Component } from 'react';
 import './App.css';
-import {  Nav, Navbar, NavItem, MenuItem, NavDropdown, FormGroup, FormControl, Button, Grid, Row, Col, Image } from 'react-bootstrap'
-import ThreeColumns from './components/three-columns'
+import {
+  Nav,
+  Navbar,
+  NavItem,
+  MenuItem,
+  NavDropdown,
+  FormGroup,
+  FormControl,
+  Button,
+  Grid,
+  Row,
+  Col,
+  Image,
+} from 'react-bootstrap';
+import ThreeColumns from './components/three-columns';
 
 class App extends Component {
-
   constructor(props) {
     super(props);
     this.state = {
       word: '',
-      boxWithItem: ''
+      boxWithItem: '',
     };
   }
 
   render() {
-
     const boxes = {
-      box1: [
-        'Loafer',
-        'Spatula'
-      ],
-      box2: [
-        'Mouse',
-        'Sunglasses'
-      ],
-      box3: [
-        'Pen',
-        'Toilet Paper'
-      ]
-    }
-
+      box1: ['Loafer', 'Spatula'],
+      box2: ['Mouse', 'Sunglasses'],
+      box3: ['Pen', 'Toilet Paper'],
+    };
 
     let show = () => {
-      //  Object.keys(boxes).map((box, index) =>  {
-      //   console.log(value: boxes[box] )
-      // })
+      Object.keys(boxes).map((box, index) => {
+        //console.log({ box, index, value: boxes[box] });
+        boxes[box].map((value, index) => {
+          console.log({ value, index });
+          return { index, value };
+        });
+      });
+      //        console.log('boxes reached', value: boxes[box] )
       for (let i = 0; i < Object.keys(boxes).length; i++) {
-        let boxArray = Object.keys(boxes)
+        let boxArray = Object.keys(boxes);
         for (let y = 0; boxes[boxArray[i]].length > y; y++) {
           //          let cappitalItems = boxes[boxArray[i]][y]
-          if( boxes[boxArray[i]][y].toUpperCase() == this.state.word ) {
-            console.log(boxArray[i])
-            this.setState({boxWithItem: boxArray[i]})
+          if (boxes[boxArray[i]][y].toUpperCase() == this.state.word) {
+            console.log(boxArray[i]);
+            this.setState({ boxWithItem: boxArray[i] });
           }
         }
       }
-    }
+    };
 
-    let there = ((this.state.boxWithItem !== '') && (this.state.word)) ? this.state.word + ' in ' + this.state.boxWithItem : 'Find it...'
+    let there =
+      this.state.boxWithItem !== '' && this.state.word
+        ? this.state.word + ' in ' + this.state.boxWithItem
+        : 'Find it...';
 
     return (
       <div className="App">
@@ -56,31 +65,48 @@ class App extends Component {
           <h1>Find my favorite item</h1>
           <Navbar.Form>
             <FormGroup>
-              <FormControl  onChange={(e) => {this.setState({word: e.target.value.toUpperCase()})}} type="text" placeholder="Search" />
+              <FormControl
+                onChange={e => {
+                  this.setState({ word: e.target.value.toUpperCase() });
+                }}
+                type="text"
+                placeholder="Search"
+              />
             </FormGroup>{' '}
-            <Button type="submit" onClick={show}>Submit</Button>
+            <Button type="submit" onClick={show}>
+              Submit
+            </Button>
           </Navbar.Form>
         </Navbar>
 
         <Grid>
-          <Row> <Col xs={6} md={4}>
-              <Image src="https://source.unsplash.com/0VGG7cqTwCo/250x250" rounded />
+          <Row>
+            {' '}
+            <Col xs={6} md={4}>
+              <Image
+                src="https://source.unsplash.com/0VGG7cqTwCo/250x250"
+                rounded
+              />
             </Col>
             <Col xs={6} md={4}>
-              <Image src="https://source.unsplash.com/z9F_yK4Nmf8/250x250" rounded />
+              <Image
+                src="https://source.unsplash.com/z9F_yK4Nmf8/250x250"
+                rounded
+              />
             </Col>
             <Col xs={6} md={4}>
-              <Image src="https://source.unsplash.com/sxNt9g77PE0/250x250" rounded />
+              <Image
+                src="https://source.unsplash.com/sxNt9g77PE0/250x250"
+                rounded
+              />
             </Col>
           </Row>
         </Grid>
         <div>
-          <h2>
-            {there}
-          </h2>
+          <h2>{there}</h2>
         </div>
       </div>
-    )
+    );
   }
 }
 
